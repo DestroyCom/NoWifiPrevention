e2fd96cf93734a2d8fc3898fbfbc819e97717450
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2024-02-13 18:22:56 +0100

    add "go anyway" button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,33 @@
 import nowifi from "@/assets/images/bruh.jpg";
-import { WifiIcon } from "./WifiIcon";
+import { WifiIcon, CrossIcon } from "./Icons";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="py-4 md:py-6 lg:py-8 bg-gray-900">
-        <div className="container flex items-center px-4 md:px-6">
-          <div className="flex items-center space-x-4">
-            <WifiIcon className="w-8 h-8 text-gray-50 " />
-            <div className="flex flex-col">
-              <h1 className="text-xl font-bold tracking-tighter text-gray-50 ">
-                Wifi ADS
-              </h1>
-              <time className="text-sm font-medium tracking-tight text-gray-300 ">
-                {new Date().toLocaleString()}
-              </time>
-            </div>
+      <header className="py-4 md:py-6 lg:py-8 bg-gray-900 flex justify-between items-center px-4 md:px-6 w-full">
+        <div className="flex items-center space-x-4">
+          <WifiIcon className="w-8 h-8 text-gray-50 " />
+          <div className="flex flex-col">
+            <h1 className="text-xl font-bold tracking-tighter text-gray-50 ">
+              Protection Wifi
+            </h1>
+            <time className="text-sm font-medium tracking-tight text-gray-300 ">
+              {new Date().toLocaleString()}
+            </time>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <CrossIcon className="hidden sm:block w-8 h-8 text-gray-50" />
+          <div className="flex flex-col">
+            <p className="text-xl font-bold tracking-tighter text-gray-50">
+              Site bloqué :
+            </p>
+            <a
+              className="text-md font-medium tracking-tight text-gray-300"
+              href={window.location.href}
+            >
+              {window.location.href}
+            </a>
           </div>
         </div>
       </header>
@@ -24,10 +36,10 @@ function App() {
           <div className="grid gap-8 text-center md:text-left md:grid-cols-2">
             <img
               alt="Meme"
-              className="rounded-xl overflow-hidden object-cover object-center mx-auto"
-              height="400"
+              className="rounded-xl overflow-hidden object-cover object-center mx-auto w-11/12"
+              //height="400"
               src={nowifi}
-              width="400"
+              //width="400"
             />
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter text-gray-900">
@@ -44,6 +56,15 @@ function App() {
                 Pour cela,{" "}
                 <span className="italic font-bold">change de réseau WiFi</span>.
               </p>
+              <button
+                onClick={() => window.location.reload()}
+                className="px-4 py-2 bg-gray-900 text-gray-50 rounded-[5px] text-center sm:text-left font-semibold tracking-tighter hover:bg-gray-700"
+              >
+                Je suis un fou, j'y vais quand même <br />{" "}
+                <span className="italic font-light">
+                  (après avoir changé de wifi)
+                </span>
+              </button>
             </div>
           </div>
         </div>
